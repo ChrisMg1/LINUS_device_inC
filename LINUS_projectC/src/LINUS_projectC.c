651b26a75949932e3c56fc03ff7855aece4d8a40
ChrisMg1 <christoph.maget@outlook.com>
2022-08-26 08:02:40 +0200

    first commit

---
+++ LINUS_projectC/src/LINUS_projectC.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : LINUS_projectC.c
+ Author      : cm
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!77c22"); /* prints !!!Hello World!!!77c */
+	return EXIT_SUCCESS;
+}
